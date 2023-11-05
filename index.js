deed7a49cf302e51d7e7531a0aa6a882ce7d4549
Andrew Wilson <adwilson12901@gmail.com>
2023-11-05 16:32:06 -0500

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+
+let num1 = 2000
+
+let num2 = 100
+
+const divide = (num1, num2) => num1 / num2
+
+function square(number) {
+    let squaredNumber = number ** 2
+    return squaredNumber
+}
+
+
+const add = (param1, param2) => param1 + param2 
